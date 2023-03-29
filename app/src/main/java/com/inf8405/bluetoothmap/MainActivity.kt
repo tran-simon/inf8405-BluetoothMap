4294546457b6c2d111f88278d72baf45f2995904
simon <tran.simon2@gmail.com>
2023-03-29 18:45:46 -0400

    Added default camera position for the map

--- app/src/main/java/com/inf8405/bluetoothmap/MainActivity.kt
+++ app/src/main/java/com/inf8405/bluetoothmap/MainActivity.kt
@@ -1,28 +1,39 @@
 package com.inf8405.bluetoothmap
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback,
     ActivityCompat.OnRequestPermissionsResultCallback,
     GoogleMap.OnMyLocationButtonClickListener {
     companion object {
-        const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val DEFAULT_ZOOM = 15f
+        private val DEFAULT_LOCATION = LatLng(45.50520841701728, -73.6131208357828)
     }
 
-    private var map: GoogleMap? = null
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    private var locationPermissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Get the map
         val mapFragment = supportFragmentManager
@@ -34,7 +45,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        map?.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationButtonClickListener(this)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, DEFAULT_ZOOM))
+
         enableMyLocation()
     }
 
@@ -44,7 +57,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
             return
         }
 
-        Toast.makeText(this, " permission xd", Toast.LENGTH_SHORT).show()
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -72,7 +84,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
             || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
             == PackageManager.PERMISSION_GRANTED
         ) {
-            map?.isMyLocationEnabled = true
+            map.isMyLocationEnabled = true
+            locationPermissionGranted = true
+            centerCamera()
             return
         }
 
@@ -82,5 +96,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
             LOCATION_PERMISSION_REQUEST_CODE
         )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun centerCamera() {
+        if (locationPermissionGranted) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), DEFAULT_ZOOM))
+                }
+            }
+        }
     }
 }
